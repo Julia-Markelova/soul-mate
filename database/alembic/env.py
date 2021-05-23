dd9aace132c0576a36efd6ed765f7ce45fec23a5
Yulia Markelova <markelova.y@digdes.com>
2021-05-23 18:50:41 +0300

    add alembic revision

---
+++ database/alembic/env.py
@@ -0,0 +1,93 @@
+import os
+import sys
+from logging.config import fileConfig
+
+from sqlalchemy import create_engine
+
+from alembic import context
+
+DATABASE_URL = os.getenv('DATABASE_URL', None)
+if DATABASE_URL is None:
+    raise ValueError("Environment variable `DATABASE_URL` must be set.")
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = None
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+exclude_tables = config.get_section('alembic:exclude').get('tables', '').split(',')
+
+
+def include_object(object, name, type_, reflected, compare_to):
+    if type_ == "table" and name in exclude_tables:
+        return False
+    else:
+        return True
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = DATABASE_URL
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        include_object=include_object
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = create_engine(DATABASE_URL)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            include_object=include_object
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
---
+++ database/alembic/versions/2021-05-23_99f7d5f140c2_new_schema.py
@@ -0,0 +1,132 @@
+"""new_schema
+
+Revision ID: 99f7d5f140c2
+Revises: 421a73ac7d57
+Create Date: 2021-05-23 14:02:24.785532
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '99f7d5f140c2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+sql = f"""
+        CREATE TYPE public.soul_status AS ENUM (
+            'UNBORN',
+            'BORN',
+            'LOST',
+            'DEAD'
+        );
+
+        CREATE TYPE public.personal_program_status AS ENUM (
+            'NEW',
+            'IN_PROGRESS',
+            'SUCCESS',
+            'FAIL'
+        );
+
+        -- tables --
+
+        CREATE TABLE public.users (
+            id                      uuid  PRIMARY KEY,
+            login                   VARCHAR (50) NOT NULL UNIQUE,
+            password                VARCHAR (50) NOT NULL
+        );
+
+        
+        CREATE TABLE public.gods (
+            id                  uuid  PRIMARY KEY,
+            user_id             uuid        REFERENCES public.users(id) ON DELETE CASCADE,
+            name                VARCHAR (256) NOT NULL
+        );
+
+        CREATE TABLE public.souls (
+            id                  uuid  PRIMARY KEY,
+            user_id             uuid        REFERENCES public.users(id) ON DELETE CASCADE,
+            status              public.soul_status,
+            is_mentor           boolean default false
+        );
+
+        CREATE TABLE public.lifes (
+            id                  uuid  PRIMARY KEY,
+            soul_id             uuid        REFERENCES public.souls(id) ON DELETE CASCADE,
+            date_of_birth       timestamp without time zone,
+            date_of_death       timestamp without time zone,
+            soul_name           VARCHAR (256),
+            soul_surname        VARCHAR (256),
+            karma               integer default 0
+        );
+
+        CREATE TABLE public.soul_relatives (
+            id                              uuid  PRIMARY KEY,
+            soul_id                         uuid        REFERENCES public.souls(id) ON DELETE CASCADE,
+            relative_id                     uuid        REFERENCES public.lifes(id) ON DELETE CASCADE,
+            notify_relative_about_soul      boolean default false
+        );
+
+        CREATE TABLE public.exercises(
+            id             uuid  PRIMARY KEY,
+            name           VARCHAR (256),
+            skill          VARCHAR (256)
+        );
+
+        CREATE TABLE public.personal_programs (
+            id                           uuid  PRIMARY KEY,
+            created_date                 timestamp without time zone,
+            soul_id                      uuid        REFERENCES public.souls(id) ON DELETE CASCADE,
+            progress_percentage          integer default 0,
+            status                       public.personal_program_status
+        );
+
+        CREATE TABLE public.program_exercises (
+            id                      uuid  PRIMARY KEY,
+            program_id              uuid        REFERENCES public.personal_programs(id) ON DELETE CASCADE,
+            exercise_id             uuid        REFERENCES public.exercises(id) ON DELETE CASCADE
+        );
+
+        CREATE TABLE public.life_sparks (
+            id                      uuid  PRIMARY KEY,
+            receive_date            timestamp   without time zone,
+            received_by             uuid        REFERENCES public.souls(id) ON DELETE CASCADE,
+            issued_by               uuid        REFERENCES public.souls(id) ON DELETE CASCADE,
+            personal_program_id     uuid        REFERENCES public.personal_programs(id) ON DELETE CASCADE
+        );
+
+        CREATE TABLE public.life_tickets (
+            id                      uuid  PRIMARY KEY,
+            receive_date            timestamp   without time zone,
+            life_spark_id           uuid        REFERENCES public.life_sparks(id) ON DELETE CASCADE,
+            is_auto_issued          boolean default false
+        );
+
+    """
+
+
+def upgrade():
+    op.execute(sql)
+
+
+def downgrade():
+    sql = f"""
+
+        DROP TABLE public.life_tickets cascade;
+        DROP TABLE public.life_sparks cascade;
+        DROP TABLE public.program_exercises cascade;
+        DROP TABLE public.personal_programs cascade;
+        DROP TABLE public.exercises cascade;
+        DROP TABLE public.soul_relatives cascade;
+        DROP TABLE public.lifes cascade;
+        DROP TABLE public.souls cascade;
+        DROP TABLE public.gods cascade;
+        DROP TABLE public.users cascade;
+
+
+        DROP TYPE public.soul_status cascade;
+        DROP TYPE public.personal_program_status cascade;
+
+    """
+
+    op.execute(sql)
