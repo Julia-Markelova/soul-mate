cd7a9cb6bd84c5d81cb58e0964b12851fc44e3e5
Yulia Markelova <markelova.y@digdes.com>
2021-05-25 15:03:55 +0300

    change id type in SoulRepository

--- src/main/java/ifmo/soulmate/demo/repositories/SoulRepository.java
+++ src/main/java/ifmo/soulmate/demo/repositories/SoulRepository.java
@@ -3,5 +3,7 @@ package ifmo.soulmate.demo.repositories;
 import ifmo.soulmate.demo.entities.Soul;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SoulRepository extends JpaRepository<Soul, Long> {
+import java.util.UUID;
+
+public interface SoulRepository extends JpaRepository<Soul, UUID> {
 }
