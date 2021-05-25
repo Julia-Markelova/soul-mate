1be3869b6a9c1bc9ac911c124722da994faf5a76
Yulia Markelova <markelova.y@digdes.com>
2021-05-25 22:12:06 +0300

    add sql query

--- src/main/java/ifmo/soulmate/demo/repositories/ISoulRelativeRepository.java
+++ src/main/java/ifmo/soulmate/demo/repositories/ISoulRelativeRepository.java
@@ -2,8 +2,13 @@ package ifmo.soulmate.demo.repositories;
 
 import ifmo.soulmate.demo.entities.SoulRelative;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ISoulRelativeRepository extends JpaRepository<SoulRelative, UUID> {
+    @Query(value = "select relative_id from soul_relatives where soul_id = ?1 and notify_relative_about_soul = true ",
+            nativeQuery = true)
+    public List<UUID> getRelativesWithAllowedNotificationsForSoulId(UUID soulId);
 }
