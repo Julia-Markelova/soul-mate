01af5f58e5f029e0d2ed330b6677b6bd6d49fb0b
Yulia Markelova <markelova.y@digdes.com>
2021-12-03 00:27:02 +0300

    disable relative controller

--- src/main/java/ifmo/soulmate/demo/controllers/RelativeController.java
+++ src/main/java/ifmo/soulmate/demo/controllers/RelativeController.java
@@ -30,23 +30,26 @@ public class RelativeController {
     LoginService loginService;
 
 
-    @GetMapping("/notifications/{relativeId}")
-    public ResponseEntity<List<MessageDto>> getNewMessages(@PathVariable String relativeId) {
-        return ResponseEntity.ok(notificationService.getNewMessagesForRelative(UUID.fromString(relativeId)));
-    }
-
-    @GetMapping("/relatives/{relativeId}/subscriptions")
-    public ResponseEntity toggleSubscribe(@PathVariable String relativeId, @RequestParam boolean enable) {
-        if (enable) {
-            notificationService.subscribe(UUID.fromString(relativeId));
-        } else {
-            notificationService.unsubscribe(UUID.fromString(relativeId));
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/relatives/{relativeId}/subscriptions-status")
-    public ResponseEntity<Boolean> getSubscriptionStatus(@PathVariable String relativeId) {
-        return ResponseEntity.ok(notificationService.getSubscriptionStatus(UUID.fromString(relativeId)));
-    }
+//    @GetMapping("/relatives/get-new-messages")
+//    public ResponseEntity<List<MessageDto>> getNewMessages(@RequestHeader("soul-token") String token) {
+//// todo
+//        return ResponseEntity.ok(notificationService.getNewMessagesForRelative(UUID.fromString(relativeId)));
+//    }
+//
+//    @GetMapping("/relatives/toggle-subscriptions")
+//    // todo
+//    public ResponseEntity toggleSubscribe(@RequestHeader("soul-token") String token, @RequestParam boolean enable) {
+//        if (enable) {
+//            notificationService.subscribe(UUID.fromString(relativeId));
+//        } else {
+//            notificationService.unsubscribe(UUID.fromString(relativeId));
+//        }
+//        return ResponseEntity.ok().build();
+//    }
+//
+//    @GetMapping("/relatives/get-subscriptions-status")
+//    // todo
+//    public ResponseEntity<Boolean> getSubscriptionStatus(@RequestHeader("soul-token") String token) {
+//        return ResponseEntity.ok(notificationService.getSubscriptionStatus(UUID.fromString(relativeId)));
+//    }
 }
