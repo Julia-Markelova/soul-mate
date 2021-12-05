f46169b920b163df24046c882288aadb098abb87
Yulia Markelova <markelova.y@digdes.com>
2021-12-05 19:04:38 +0300

    annotations

--- src/main/java/ifmo/soulmate/demo/controllers/MentorController.java
+++ src/main/java/ifmo/soulmate/demo/controllers/MentorController.java
@@ -14,15 +14,15 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
 public class MentorController {
 
     @Autowired
